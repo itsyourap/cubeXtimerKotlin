788495abd800d8a89170ccce00f0ca11c34422a8
Ankan Pal <itsyourap@gmail.com>
2023-12-18 14:16:12 +0530

    Optimise imports

--- app/src/main/java/com/example/dark_x_timer_kotlin/ui/counter/historySheet/HIstorySheetUI.kt
+++ app/src/main/java/com/example/dark_x_timer_kotlin/ui/counter/historySheet/HIstorySheetUI.kt
@@ -1,17 +1,11 @@
 package com.example.dark_x_timer_kotlin.ui.counter.historySheet
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Card
@@ -23,7 +17,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
